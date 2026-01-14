5e25a7bee20c8e79e116fa04b289deb339c916da
mWaleedh <187419608+mWaleedh@users.noreply.github.com>
2026-01-14 08:07:18 +0500

    Add git push script

---
+++ git_push.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+	echo "Usage: $0 <file_name> [commit_message]"
+	exit 1
+fi
+
+if ! git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+	echo "Not inside a git repo"
+	exit 1
+fi
+
+if git diff --quiet; then
+    echo "No changes to commit"
+    exit 0
+fi
+
+REPO="$(basename '$(git rev-parse --show-toplevel)')"
+FILE=$1
+MESSAGE=$2
+
+if [ -f "$FILE" ]; then
+	git add "$FILE"
+else
+	echo "$FILE does not exist"
+	exit 1
+fi
+
+if [ -z "$MESSAGE" ]; then
+	MESSAGE="update $FILE"
+fi
+
+git commit -m "$MESSAGE"
+
+git push
+
+echo "Pushed $FILE to $REPO"
